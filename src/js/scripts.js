400f57026f3d90f2b4bb3a2aaa60fc7053dc452c
Ben Kiggen and Colin Cook <colin@>
2018-09-26 16:46:22 -0700

    random movement generator and repeats

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,54 +1,70 @@
+// BACK END
+
+function chooseSpooky() {
+  var choiceNumber = Math.floor(Math.random() * 5);
+  var spookies = ['bat', 'horse', 'hand1', 'hand2', 'hand3'];
+  var choice = spookies[choiceNumber]
+  return choice;
+}
+
+
+
 
 
 $(document).ready(function(){
-
   $(".bat").click(function(){
     var elementInfo = document.getElementById("bat");
-    var elementName = ".bat";
+    var elementName = "bat";
     drop(elementInfo, elementName);
   });
 
   $(".horse").click(function(){
     var elementInfo = document.getElementById("horse");
-    var elementName = ".horse";
+    var elementName = "horse";
     drop(elementInfo, elementName);
   });
 
   $(".hand1").click(function(){
     var elementInfo = document.getElementById("hand1");
-    var elementName = ".hand1";
+    var elementName = "hand1";
     drop(elementInfo, elementName);
   });
 
   $(".hand2").click(function(){
     var elementInfo = document.getElementById("hand2");
-    var elementName = ".hand2";
+    var elementName = "hand2";
     drop(elementInfo, elementName);
   });
 
   $(".hand3").click(function(){
     var elementInfo = document.getElementById("hand3");
-    var elementName = ".hand3";
+    var elementName = "hand3";
     drop(elementInfo, elementName);
   });
 
   function drop(idname, className){
     var rect = idname.getBoundingClientRect();
-    $(className).addClass("stop");
-    var left = rect.left;
+    $("." + className).addClass("stop");
+    var left = rect.left - 8;
     var top = rect.top;
-    $(className).animate({
+    $("." + className).animate({
       top: top,
       left: left
     }, 1, function(){
     });
-    $(className).animate({
+    $("." + className).animate({
       top: "+=2000"
     }, 1000, function(){
     });
-    $(className).addClass("nopoint");
+    $("." + className).addClass("nopoint");
+    $("." + className).removeClass(className + "-move");
+    $("." + className).removeClass("stop");
     setTimeout(function(){
-      $(className).removeClass("nopoint");
+      $("." + className).removeClass("nopoint");
     }, 1000);
   };
+  window.setInterval(function(){
+    var choice = chooseSpooky();
+    $("." + choice).addClass(choice + "-move");
+  }, 1000);
 });
